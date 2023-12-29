695b880830402cdf5d2ac467b239dcdf3ae3abd5
PerkGuacamole <evanunmann@gmail.com>
2023-12-29 11:05:01 -0500

    Implementing Modulo Rejection Sampling

--- background.js
+++ background.js
@@ -8,54 +8,61 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+const UINT32_MAX = 4294967295; // 2**32 - 1
+
 function generateSecureRandom(max) {
-    const array = new Uint32Array(1);
+  const array = new Uint32Array(1);
+  const cutoff = UINT32_MAX - (UINT32_MAX % max);
+  while (true) {
     crypto.getRandomValues(array);
-    return array[0] % max;
+    if (array[0] < cutoff) {
+      return array[0] % max;
+    }
+  }
 }
 
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = generateSecureRandom(i + 1);
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = generateSecureRandom(i + 1);
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 function generateStrongPassword() {
-    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-    const numbers = '0123456789';
-    const specialChars = '!@#$%^&*()_+{}:"<>?[];,./\'';
-    const allChars = uppercaseChars + lowercaseChars + numbers + specialChars;
-    let password = [];
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const numbers = '0123456789';
+  const specialChars = '!@#$%^&*()_+{}:"<>?[];,./\'';
+  const allChars = uppercaseChars + lowercaseChars + numbers + specialChars;
+  let password = [];
 
-    function getRandomChar(str) {
-        return str[generateSecureRandom(str.length)];
-    }
+  function getRandomChar(str) {
+    return str[generateSecureRandom(str.length)];
+  }
 
-    const minCharInstances = 2;
-    let i = 0;
-    for (; i < minCharInstances; i++) {
-        password.push(getRandomChar(uppercaseChars));
-        password.push(getRandomChar(lowercaseChars));
-        password.push(getRandomChar(numbers));
-        password.push(getRandomChar(specialChars));
-    }
+  const minCharInstances = 2;
+  let i = 0;
+  for (; i < minCharInstances; i++) {
+    password.push(getRandomChar(uppercaseChars));
+    password.push(getRandomChar(lowercaseChars));
+    password.push(getRandomChar(numbers));
+    password.push(getRandomChar(specialChars));
+  }
 
-    const passwordLength = 16;
-    for (; i < passwordLength; i++) {
-        password.push(getRandomChar(allChars));
-    }
+  const passwordLength = 16;
+  for (; i < passwordLength; i++) {
+    password.push(getRandomChar(allChars));
+  }
 
-    password = shuffleArray(password);
+  password = shuffleArray(password);
 
-    return password.join('');
+  return password.join('');
 }
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === generatePasswordId) {
-    let generatedPassword = generateStrongPassword();
+    const generatedPassword = generateStrongPassword();
 
     // New API usage
     chrome.scripting.executeScript({
